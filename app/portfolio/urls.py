af471477c046d3d677a1e60c644246bd007a8396
BrunoTerres <brunoeterres@gmail.com>
2021-02-18 12:55:21 -0300

    Main features added to docker dev enviroment

--- app/portfolio/urls.py
+++ app/portfolio/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
-]
+    path('', RedirectView.as_view(url='home/', permanent=True)),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if bool(settings.DEBUG):
